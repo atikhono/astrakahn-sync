55e989187d8928272b8e873351050efb04834b9f
Anna Tikhonova <anna.m.tikhonova@gmail.com>
2015-09-16 16:47:20 +0300

    Synchroniser parser and improved macro management for backend.
       * sync/parser.py: Sync parser.
       * sync/compiler.py: Call parser. Manage macros in a different way.

--- sync/compiler.py
+++ sync/compiler.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+import imp
+import re
+
 from . import lexer as sync_lexer
+from . import parser as sync_parser
 
 
 def macro_subst(code, macros):
@@ -29,6 +35,37 @@ def parse(code, macros={}):
     #if macros:
     #    code = macro_subst(code, macros)
 
-    lexer = sync_lexer.build()
+    code_lines = code.split('\n')
 
-    return lexer
+    configs = {}
+
+    # Read parameters from the header.
+    for i, line in enumerate(code_lines):
+        line = line.strip()
+        if line.startswith('@'):
+            match = re.findall("\@([A-Za-z_][\w_]*)(?:(?:\s*)\=(?:\s*)([\w\d]+))?$", line)
+            if not match:
+                continue
+            else:
+                name, value = match[0]
+
+                if type(value) == str and value.isdigit():
+                    value = int(value)
+
+                configs[name] = macros.get(name, value)
+
+        elif line.startswith('synch'):
+            code = "\n".join(code_lines[i:])
+
+    sync_parser.configs = configs
+
+    for name in configs:
+        sync_parser.config_nodes[name] = []
+
+    lexer = sync_lexer.build()
+    parser = sync_parser.build()
+
+    sync_ast = parser.parse(code, lexer=lexer)
+    sync_ast.configs = sync_parser.config_nodes
+
+    return sync_ast
